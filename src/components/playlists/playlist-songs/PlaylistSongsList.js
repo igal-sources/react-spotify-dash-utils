34ddf01fbd20b9e2f6371f51ff1f4d5907fe40fa
Igal Leibovich <igal2025@gmail.com>
2020-03-22 16:24:47 +0200

    Handle TrackItem component in playlists 1.

--- src/components/playlists/playlist-songs/PlaylistSongsList.js
+++ src/components/playlists/playlist-songs/PlaylistSongsList.js
@@ -1,6 +1,4 @@
 import React from "react";
-import { useDispatch } from "react-redux";
-import allActions from "actions";
 import { Grid } from "semantic-ui-react";
 import TrackItem from "../../tracks/TrackItem";
 import SongsCard from "../../songs/songs-card/SongsCard";
@@ -9,24 +7,6 @@ import "./playlist-songs-list.scss";
 const PlaylistSongsList = props => {
   let { playlist } = props.location.state;
   console.log("PlaylistSongsList - props: ", props.location.state);
-  const dispatch = useDispatch();
-
-  const handleClickPlayTrack = trackUrl =>
-    dispatch(allActions.songActions.playSong({ songId: trackUrl }));
-
-  const getImage = track => {
-    if (track.album.images.length === 0) {
-      return undefined;
-    }
-    if (track.album.images.length === 1) {
-      return track.album.images[0].url;
-    }
-    const imageItem = track.album.images.filter(item => {
-      return item.height === 64;
-    });
-
-    return imageItem && imageItem[0].url;
-  };
 
   return (
     <div className="SongsContainer-main">
